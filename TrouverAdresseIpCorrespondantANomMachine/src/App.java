af590dce51db47a250c4a84b819f52c037bd6602
Verlaine KOUWE <verlaine_k@outlook.com>
2023-12-16 13:58:23 +0100

    inclusion de la classe Scanner

--- TrouverAdresseIpCorrespondantANomMachine/src/App.java
+++ TrouverAdresseIpCorrespondantANomMachine/src/App.java
@@ -1,20 +1,67 @@
+// import java.net.*;
+
+// public class App {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//         if (args.length == 0) {
+//             System.err.println("Please provide a hostname as a command-line argument.");
+//             return;
+//         }
+//         InetAddress adresse;
+//         try {
+//             adresse = InetAddress.getByName(args[0]);
+//             System.out.println("Nom: "+ args[0]+", IP: " + adresse.getHostAddress());       
+//         } catch (UnknownHostException e) {
+//             System.err.println(args[0]+" est inconnu\n");
+//         }
+      
+//     }
+// }
+
+
+// import java.net.*;
+
+// public class App {
+//     public static void main(String[] args) {
+//         if (args.length == 0) {
+//             System.err.println("Veuillez fournir un nom de machine en argument.");
+//             return;
+//         }
+
+//         String nomMachine = args[0];
+//         try {
+//             InetAddress adresse = InetAddress.getByName(nomMachine);
+//             System.out.println("Nom: " + nomMachine + ", IP: " + adresse.getHostAddress());
+//         } catch (UnknownHostException e) {
+//             System.err.println(nomMachine + " est inconnu.");
+//         }
+//     }
+// }
+
+
 import java.net.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        if (args.length == 0) {
-            System.err.println("Please provide a hostname as a command-line argument.");
-            return;
+        String nomMachine;
+
+        // Vérifie si un argument est fourni en ligne de commande
+        if (args.length > 0) {
+            nomMachine = args[0];
+        } else {
+            try (// Si aucun argument n'est fourni, demandez à l'utilisateur de saisir le nom de la machine
+            Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Veuillez saisir un nom de machine : ");
+                nomMachine = scanner.nextLine();
+            }
         }
-        InetAddress adresse;
+
         try {
-            adresse = InetAddress.getByName(args[0]);
-            System.out.println("Nom: "+ args[0]+", IP: " + adresse.getHostAddress());       
+            InetAddress adresse = InetAddress.getByName(nomMachine);
+            System.out.println("Nom: " + nomMachine + ", IP: " + adresse.getHostAddress());
         } catch (UnknownHostException e) {
-            System.err.println(args[0]+" est inconnu\n");
+            System.err.println(nomMachine + " est inconnu.");
         }
-      
     }
 }
