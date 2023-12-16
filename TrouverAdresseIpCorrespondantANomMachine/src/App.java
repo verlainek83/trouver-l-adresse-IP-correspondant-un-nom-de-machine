f3354d8ecace81e254b56f0cae297ca685c52482
Verlaine KOUWE <verlaine_k@outlook.com>
2023-12-16 13:46:24 +0100

    écriture du code d'affichage l'adresse IP

--- TrouverAdresseIpCorrespondantANomMachine/src/App.java
+++ TrouverAdresseIpCorrespondantANomMachine/src/App.java
@@ -1,5 +1,20 @@
+import java.net.*;
+import java.io.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+        if (args.length == 0) {
+            System.err.println("Please provide a hostname as a command-line argument.");
+            return;
+        }
+        InetAddress adresse;
+        try {
+            adresse = InetAddress.getByName(args[0]);
+            System.out.println("Nom: "+ args[0]+", IP: " + adresse.getHostAddress());       
+        } catch (UnknownHostException e) {
+            System.err.println(args[0]+" est inconnu\n");
+        }
+      
     }
 }
